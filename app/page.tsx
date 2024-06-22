f0b124a4629f26bf7693ccd120737d8f3f311b2d
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-06-22 18:02:03 -0300

    UI adaptation to XP library

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ import React, { useState, useEffect, useRef, SetStateAction } from 'react';
 import Image from "next/image";
 import backgroundImage from '@/app/assets/background.jpg';
 import windowsButton from '@/app/assets/menu-button.png';
-import minimizeButton from '@/app/assets/minimize-button.png';
-import maximizeButton from '@/app/assets/maximize-button.png';
-import closeButton from '@/app/assets/close-button.png';
 import { sendMessageToOpenAI } from '@/app/utils/openai';
 
 export default function Home() {
@@ -54,52 +51,49 @@ export default function Home() {
   return (
     <main className="h-screen">
       <Image src={backgroundImage} alt="Windows XP background image" className="h-screen"></Image>
-        <div className="fixed rounded-t-lg top-[15%] left-[20%] h-[60%] w-[60%] shadow-[11px_15px_30px_-5px_rgba(0,0,0,0.75)]">
-          <div className="h-full w-full rounded-t-lg bg-azul border-x border-y border-azul-oscuro">
-            <div className="h-[5%] w-[100%] mb-[0.2%] rounded-t-lg 
-            bg-gradient-to-b from-0% from-azul via-10% via-celeste to-20% to-azul">
-              <div className="flex h-full w-full pr-[0.15rem]">
-                <span className="flex pl-1 pt-1 items-center text-slate-100 text-[14px] w-[10%] text-shadow">Chat GPT</span>
-                <div className="flex h-full w-[90%] justify-end items-end">
-                  <Image src={minimizeButton} alt="Windows XP minimize button" className="flex h-[80%] w-[2%]"></Image>
-                  <Image src={maximizeButton} alt="Windows XP maximize button" className="flex h-[80%] w-[2%]"></Image>
-                  <Image src={closeButton} alt="Windows XP close button" className="flex h-[80%] w-[2%] mr-[2px]"></Image>
-                </div>
+        <div className="fixed top-[15%] left-[20%] h-[60%] w-[60%] shadow-[11px_15px_30px_-5px_rgba(0,0,0,0.75)]">
+          <div className="window h-full w-full">
+            <div className="title-bar !h-[30px]">
+              <div className="title-bar-text">Chat GPT</div>
+              <div className="title-bar-controls">
+                <button aria-label="Minimize"></button>
+                <button aria-label="Maximize"></button>
+                <button aria-label="Close"></button>
               </div>
             </div>
-            <div className="flex flex-col justify-end items-center h-[94.3%] w-[99.6%] bg-zinc-300 ml-[0.2%] mb-[0.5%] border-t-2 border-b border-x border-azul-oscuro">
-                <div className="grid h-full w-full overflow-y-scroll mt-1">
-                  <ul className="grid self-start">
-                  { messages.length > 0 &&
+            <div className="window-body !h-[calc(100%-30px)] !ml-[3px] !mr-[3px] !mt-[1px] !mb-[0px]">
+              <div className="grid h-full w-full">
+                <ul className="grid h-full w-full self-start overflow-y-scroll">
+                  {/* { messages.length > 0 &&
                     messages.map((message, i) => 
                       <li id={String(i)} key={i} className="bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2 odd:justify-self-end even:justify-self-start">
                         {message}
                       </li>
                     )
-                  }
+                  } */}
 
-                  {/* <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li>
-                  <li className="self-start justify-self-start bg-gray-700 text-white rounded-md w-fit h-fit p-3 ml-1 my-1"></li>
-                  <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li>
-                  <li className="self-start justify-self-start bg-gray-700 text-white rounded-md w-fit h-fit p-3 ml-1 my-1"></li>
-                  <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li>
-                  <li className="self-start justify-self-start bg-gray-700 text-white rounded-md w-fit h-fit p-3 ml-1 my-1"></li>
-                  <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li>
-                  <li className="self-start justify-self-start bg-gray-700 text-white rounded-md w-fit h-fit p-3 ml-1 my-1"></li>
-                  <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li>
-                  <li className="self-start justify-self-start bg-gray-700 text-white rounded-md w-fit h-fit p-3 ml-1 my-1"></li>
-                  <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li> */}
-                  </ul>
+                  <li className="justify-self-end bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore expedita, maiores amet eos et harum autem, aliquam in, officiis cum sint? Laborum, vel illo. Repudiandae et quibusdam perspiciatis vero quisquam.</li>
+                  <li className="justify-self-start bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nostrum impedit pariatur voluptatum laboriosam voluptatibus nisi quibusdam in fugiat velit, exercitationem perferendis ratione, dolores laborum quisquam quo culpa, maxime reiciendis!</li>
+                  <li className="justify-self-end bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, perspiciatis itaque dolorem unde, eum mollitia nisi fuga repellat earum accusamus voluptatibus magnam laborum. Nobis laudantium perferendis, optio suscipit neque cum?</li>
+                  <li className="justify-self-start bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, perspiciatis itaque dolorem unde, eum mollitia nisi fuga repellat earum accusamus voluptatibus magnam laborum. Nobis laudantium perferendis, optio suscipit neque cum?</li>
+                  <li className="justify-self-end bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum repellendus repudiandae minima rem culpa. Repudiandae iure libero minima. Quo tenetur cumque incidunt rem eligendi molestiae dolore facilis porro non? Dolorem!</li>
+                  <li className="justify-self-start bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit laboriosam quaerat, natus adipisci corporis molestiae cumque vel repudiandae sapiente! Veritatis perspiciatis dignissimos mollitia odit provident doloribus perferendis est, quaerat assumenda!</li>
+                  <li className="justify-self-end bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum repellendus repudiandae minima rem culpa. Repudiandae iure libero minima. Quo tenetur cumque incidunt rem eligendi molestiae dolore facilis porro non? Dolorem!</li>
+                  <li className="justify-self-start bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit laboriosam quaerat, natus adipisci corporis molestiae cumque vel repudiandae sapiente! Veritatis perspiciatis dignissimos mollitia odit provident doloribus perferendis est, quaerat assumenda!</li>
+                  <li className="justify-self-end bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum repellendus repudiandae minima rem culpa. Repudiandae iure libero minima. Quo tenetur cumque incidunt rem eligendi molestiae dolore facilis porro non? Dolorem!</li>
+                  <li className="justify-self-start bg-gray-700 text-white rounded-md w-fit max-w-xl h-fit p-3 m-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit laboriosam quaerat, natus adipisci corporis molestiae cumque vel repudiandae sapiente! Veritatis perspiciatis dignissimos mollitia odit provident doloribus perferendis est, quaerat assumenda!</li>
+                </ul>
+                <div className="grid self-end my-1">
+                  <div className="flex mx-1">
+                      <input type="text" className="flex h-6 w-[90%]" value={input} onChange={(e) => handleChange(e.target.value)} onKeyDown={(e) => handleKeyDown(e.key)} />
+                      <button className="inline-flex h-6 w-[10%] ml-1 items-center justify-center" onClick={handleClick}>Enviar</button>
+                  </div>
                 </div>
-              <div className="flex flex-row w-full justify-start mt-2">
-                <input type="text" className="flex h-6 w-5/6 border border-teal-700 mx-1 mb-1 px-1 text-black" value={input} onChange={(e) => handleChange(e.target.value)} onKeyDown={(e) => handleKeyDown(e.key)} />
-                <button type="button" className="inline-flex h-6 w-1/6 mx-1 text-black items-center justify-center border-2 
-                rounded-md border-teal-900 bg-gris-claro ring-inset ring-offset-blue-400 ring-2" onClick={handleClick}>Enviar</button>
               </div>
             </div>
           </div>
         </div>
-        <footer className="w-full h-10 flex flex-row absolute bottom-0 left-0 bg-gradient-to-b from-0% from-azul via-10% via-celeste
+        <footer className="w-full h-8 flex flex-row absolute bottom-0 left-0 bg-gradient-to-b from-0% from-azul via-10% via-celeste
         to-20% to-azul">
           <Image src={windowsButton} alt="Windows XP menu button"/>
         </footer>
