a25812d69354574dc8f4a0250d38ba72f1b08c64
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-06-22 14:07:55 -0300

    Chat window redesign

--- app/page.tsx
+++ app/page.tsx
@@ -54,22 +54,22 @@ export default function Home() {
   return (
     <main className="h-screen">
       <Image src={backgroundImage} alt="Windows XP background image" className="h-screen"></Image>
-        <div className="fixed rounded-t-lg top-[25%] left-[37%] h-[40%] w-[25%] shadow-[11px_15px_30px_-5px_rgba(0,0,0,0.75)]">
+        <div className="fixed rounded-t-lg top-[15%] left-[20%] h-[60%] w-[60%] shadow-[11px_15px_30px_-5px_rgba(0,0,0,0.75)]">
           <div className="h-full w-full rounded-t-lg bg-azul border-x border-y border-azul-oscuro">
-            <div className="h-[7.5%] w-[100%] rounded-t-lg 
+            <div className="h-[5%] w-[100%] mb-[0.2%] rounded-t-lg 
             bg-gradient-to-b from-0% from-azul via-10% via-celeste to-20% to-azul">
-              <div className="flex justify-end items-center h-full w-full pr-[0.15rem] ">
-                <Image src={minimizeButton} alt="Windows XP minimize button" className="flex h-[85%] w-[5%]"></Image>
-                <Image src={maximizeButton} alt="Windows XP maximize button" className="flex h-[85%] w-[5%]"></Image>
-                <Image src={closeButton} alt="Windows XP close button" className="flex h-[85%] w-[5%]"></Image>
+              <div className="flex justify-end items-end h-full w-full pr-[0.15rem] ">
+                <Image src={minimizeButton} alt="Windows XP minimize button" className="flex h-[80%] w-[2%]"></Image>
+                <Image src={maximizeButton} alt="Windows XP maximize button" className="flex h-[80%] w-[2%]"></Image>
+                <Image src={closeButton} alt="Windows XP close button" className="flex h-[80%] w-[2%] mr-[2px]"></Image>
               </div>
             </div>
-            <div className="flex flex-col justify-end items-center h-[91.3%] w-[98.8%] bg-zinc-300 ml-[0.5%] mb-[0.5%] border-t-2 border-b border-x border-azul-oscuro">
+            <div className="flex flex-col justify-end items-center h-[94.3%] w-[99.6%] bg-zinc-300 ml-[0.2%] mb-[0.5%] border-t-2 border-b border-x border-azul-oscuro">
                 <div className="grid h-full w-full overflow-y-scroll">
                   <ul className="grid gap-4 self-start">
                   { messages.length > 0 &&
                     messages.map((message, i) => 
-                      <li id={String(i)} key={i} className="bg-gray-700 text-white rounded-md w-fit h-fit p-3 my-1 odd:justify-self-end mr-1 even:justify-self-start ml-1">
+                      <li id={String(i)} key={i} className="bg-gray-700 text-white rounded-md w-fit h-fit p-3 m-2 odd:justify-self-end even:justify-self-start">
                         {message}
                       </li>
                     )
@@ -88,9 +88,9 @@ export default function Home() {
                   <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li> */}
                   </ul>
                 </div>
-              <div className="flex flex-row w-full justify-start mt-1">
-                <input type="text" className="flex h-6 w-3/4 border border-teal-700 mx-1 mb-1 px-1 text-black" value={input} onChange={(e) => handleChange(e.target.value)} onKeyDown={(e) => handleKeyDown(e.key)} />
-                <button type="button" className="flex h-6 w-1/4 mx-1 text-black text-center justify-center border-2 
+              <div className="flex flex-row w-full justify-start mt-2">
+                <input type="text" className="flex h-6 w-5/6 border border-teal-700 mx-1 mb-1 px-1 text-black" value={input} onChange={(e) => handleChange(e.target.value)} onKeyDown={(e) => handleKeyDown(e.key)} />
+                <button type="button" className="inline-flex h-6 w-1/6 mx-1 text-black items-center justify-center border-2 
                 rounded-md border-teal-900 bg-gris-claro ring-inset ring-offset-blue-400 ring-2" onClick={handleClick}>Enviar</button>
               </div>
             </div>
