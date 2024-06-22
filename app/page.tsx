9ed94ea389b518531b62815a92833abb46739507
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-06-22 00:56:18 -0300

    OpenAI API interaction configured + messages styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,86 +1,61 @@
 'use client';
 
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, SetStateAction } from 'react';
 import Image from "next/image";
 import backgroundImage from '@/app/assets/background.jpg';
 import windowsButton from '@/app/assets/menu-button.png';
 import minimizeButton from '@/app/assets/minimize-button.png';
 import maximizeButton from '@/app/assets/maximize-button.png';
 import closeButton from '@/app/assets/close-button.png';
+import { sendMessageToOpenAI } from '@/app/utils/openai';
 
 export default function Home() {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const boxRef = useRef<HTMLDivElement>(null);
-
-  const isClicked = useRef<boolean>(false);
-
-  const coords = useRef<{
-    startX: number,
-    startY: number,
-    lastX: number,
-    lastY: number
-  }>({
-    startX: 0,
-    startY: 0,
-    lastX: 0,
-    lastY: 0
-  })
+  const [input, setInput] = useState<string>('');
+  const [messages, setMessages] = useState<string[]>([]);
+  const messagesLengthRef = useRef<number>(0);
 
   useEffect(() => {
-    if (!boxRef.current || !containerRef.current) return;
+    scrollToLastMessage();
+  }, [messages]);
 
-    const box = boxRef.current;
-    const container = containerRef.current;
-
-
-    const onMouseDown = (e: MouseEvent) => {
-      isClicked.current = true;
-      coords.current.startX = e.clientX;
-      coords.current.startY = e.clientY;
+  function handleChange(value: SetStateAction<string>) {
+    setInput(value);
+  }
+  
+  async function handleSendInput() {
+    setMessages(prevState => [ ...prevState, input ]);
+    messagesLengthRef.current += 1;
+    let res = await sendMessageToOpenAI(input);
+    if(res !== null) {
+      setMessages(prevState => [ ...prevState, res ]);
+      messagesLengthRef.current += 1;
     }
+    setInput('');
+  }
 
-    const onMouseUp = (e: MouseEvent) => {
-      isClicked.current = false;
-      coords.current.lastX = box.offsetLeft;
-      coords.current.lastY = box.offsetTop;
+  function handleKeyDown(key: string) {
+    if(key === 'Enter') {
+      handleSendInput();
     }
+  }
 
-    const onMouseMove = (e: MouseEvent) => {
-      if (!isClicked.current) return;
+  function handleClick() {
+    handleSendInput();
+  }
 
-      const nextX = e.clientX - coords.current.startX + coords.current.lastX;
-      const nextY = e.clientY - coords.current.startY + coords.current.lastY;
-
-      box.style.top = `${nextY}px`;
-      box.style.left = `${nextX}px`;
-    }
-
-    box.addEventListener('mousedown', onMouseDown);
-    box.addEventListener('mouseup', onMouseUp);
-    container.addEventListener('mousemove', onMouseMove);
-    container.addEventListener('mouseleave', onMouseUp);
-
-    const cleanup = () => {
-      box.removeEventListener('mousedown', onMouseDown);
-      box.removeEventListener('mouseup', onMouseUp);
-      container.removeEventListener('mousemove', onMouseMove);
-      container.removeEventListener('mouseleave', onMouseUp);
-    }
-
-    return cleanup;
-  }, [])
+  function scrollToLastMessage() {
+    if(messagesLengthRef.current === 0) return;
+    document.getElementById(String(messagesLengthRef.current - 1))!.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end'
+    })
+  }
 
   return (
-    <div ref={containerRef} className="h-screen w-screen flex items-center justify-center overflow-hidden">
-      <div ref={boxRef} className="w-24 h-24 bg-red-500 flex cursor-pointer"></div>
-    </div>
-  );
-}
-
-{/* <main className="h-screen">
+    <main className="h-screen">
       <Image src={backgroundImage} alt="Windows XP background image" className="h-screen"></Image>
-        <div ref={containerRef} className="fixed rounded-t-lg top-[25%] left-[37%] h-[40%] w-[25%] shadow-[11px_15px_30px_-5px_rgba(0,0,0,0.75)]">
-          <div ref={boxRef}className="h-full w-full rounded-t-lg bg-azul border-x border-y border-azul-oscuro">
+        <div className="fixed rounded-t-lg top-[25%] left-[37%] h-[40%] w-[25%] shadow-[11px_15px_30px_-5px_rgba(0,0,0,0.75)]">
+          <div className="h-full w-full rounded-t-lg bg-azul border-x border-y border-azul-oscuro">
             <div className="h-[7.5%] w-[100%] rounded-t-lg 
             bg-gradient-to-b from-0% from-azul via-10% via-celeste to-20% to-azul">
               <div className="flex justify-end items-center h-full w-full pr-[0.15rem] ">
@@ -89,11 +64,42 @@ export default function Home() {
                 <Image src={closeButton} alt="Windows XP close button" className="flex h-[85%] w-[5%]"></Image>
               </div>
             </div>
-            <div className="h-[91.3%] w-[98.8%] bg-zinc-300 ml-[0.5%] mb-[0.5%] border-t-2 border-b border-x border-azul-oscuro"></div>
+            <div className="flex flex-col justify-end items-center h-[91.3%] w-[98.8%] bg-zinc-300 ml-[0.5%] mb-[0.5%] border-t-2 border-b border-x border-azul-oscuro">
+                <div className="grid h-full w-full overflow-y-scroll">
+                  <ul className="grid gap-4 self-start">
+                  { messages.length > 0 &&
+                    messages.map((message, i) => 
+                      <li id={String(i)} key={i} className="bg-gray-700 text-white rounded-md w-fit h-fit p-3 my-1 odd:justify-self-end mr-1 even:justify-self-start ml-1">
+                        {message}
+                      </li>
+                    )
+                  }
+
+                  {/* <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li>
+                  <li className="self-start justify-self-start bg-gray-700 text-white rounded-md w-fit h-fit p-3 ml-1 my-1"></li>
+                  <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li>
+                  <li className="self-start justify-self-start bg-gray-700 text-white rounded-md w-fit h-fit p-3 ml-1 my-1"></li>
+                  <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li>
+                  <li className="self-start justify-self-start bg-gray-700 text-white rounded-md w-fit h-fit p-3 ml-1 my-1"></li>
+                  <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li>
+                  <li className="self-start justify-self-start bg-gray-700 text-white rounded-md w-fit h-fit p-3 ml-1 my-1"></li>
+                  <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li>
+                  <li className="self-start justify-self-start bg-gray-700 text-white rounded-md w-fit h-fit p-3 ml-1 my-1"></li>
+                  <li className="self-start justify-self-end bg-gray-700 text-white rounded-md w-fit h-fit p-3 mr-1 my-1"></li> */}
+                  </ul>
+                </div>
+              <div className="flex flex-row w-full justify-start mt-1">
+                <input type="text" className="flex h-6 w-3/4 border border-teal-700 mx-1 mb-1 px-1 text-black" value={input} onChange={(e) => handleChange(e.target.value)} onKeyDown={(e) => handleKeyDown(e.key)} />
+                <button type="button" className="flex h-6 w-1/4 mx-1 text-black text-center justify-center border-2 
+                rounded-md border-teal-900 bg-gris-claro ring-inset ring-offset-blue-400 ring-2" onClick={handleClick}>Enviar</button>
+              </div>
+            </div>
           </div>
         </div>
         <footer className="w-full h-10 flex flex-row absolute bottom-0 left-0 bg-gradient-to-b from-0% from-azul via-10% via-celeste
         to-20% to-azul">
           <Image src={windowsButton} alt="Windows XP menu button"/>
         </footer>
-    </main> */}
+    </main>
+  );
+}
