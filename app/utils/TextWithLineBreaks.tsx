6534e54b82b2869608dc36b516e5a29635da0c7e
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-09-12 00:30:22 -0300

    Update messages component -> images by URL accepted

--- app/utils/TextWithLineBreaks.tsx
+++ app/utils/TextWithLineBreaks.tsx
@@ -1,15 +1,25 @@
 import { Fragment } from 'react';
+import Image from "next/image";
 
 type Props = {
-    text: string
+  text: string
 }
 
 export function TextWithLineBreaks(props: Props) {
   const textWithBreaks = props.text.split('\n').map((text, index) => (
-    <Fragment key={index}>
-      {text}
-      <br />
-    </Fragment>
+    <>
+    { props.text.includes('http') ?
+      <Fragment key={index}>
+        <Image src={props.text} alt="ChatGPT generated image" width={1024} height={1024} />
+        <br />
+      </Fragment>  
+      :
+      <Fragment key={index}>
+        {text}
+        <br />
+      </Fragment>
+    }
+    </>
   ));
 
   return <>{textWithBreaks}</>;
