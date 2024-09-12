4cbfdfdfe15093084aa2bd386ef3e02705670857
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-09-12 00:29:26 -0300

    Update openai lib -> generate image API function

--- app/lib/openai.tsx
+++ app/lib/openai.tsx
@@ -25,3 +25,15 @@ export async function sendMessageToOpenAI(message: string) {
     
     return chatCompletion.choices[0].message.content;
 }
+
+export async function generateImage(message: string) {
+    const response = await openai.images.generate({
+        model: "dall-e-3",
+        prompt: `${message}`,
+        size: "1024x1024",
+        quality: "standard",
+        n: 1,
+    });
+    
+    return response.data[0].url;
+}
